6507687b501f4c78916f38bb02eae17ed6e7d45f
Ayaz Salikhov <asalikhov@ripple.com>
2026-03-25 19:39:54 +0000

    chore: Commit with a conflict

--- conanfile.py
+++ conanfile.py
@@ -141,6 +141,7 @@ class Xrpl(ConanFile):
         transitive_headers_opt = (
             {"transitive_headers": True} if conan_version.split(".")[0] == "2" else {}
         )
+        # Wow, what a conflict!
         self.requires("boost/1.90.0", force=True, **transitive_headers_opt)
         self.requires("date/3.0.4", **transitive_headers_opt)
         self.requires("lz4/1.10.0", force=True)
