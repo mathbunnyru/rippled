2df6bbfd9f6498fd45e014f0db69b47d0adfe44f
Ayaz Salikhov <asalikhov@ripple.com>
2026-06-29 12:14:47 +0100

    Keep list sorted

--- conanfile.py
+++ conanfile.py
@@ -131,15 +131,15 @@ class Xrpl(ConanFile):
     def requirements(self):
         self.requires("boost/1.91.0", force=True, transitive_headers=True)
         self.requires("date/3.0.4", transitive_headers=True)
+        if self.options.jemalloc:
+            self.requires("jemalloc/5.3.1")
         self.requires("lz4/1.10.0", force=True)
         self.requires("mpt-crypto/0.4.0-rc2", transitive_headers=True)
         self.requires("protobuf/6.33.5", force=True)
-        self.requires("secp256k1/0.7.1", transitive_headers=True)
-        self.requires("sqlite3/3.53.0", force=True)
-        if self.options.jemalloc:
-            self.requires("jemalloc/5.3.1")
         if self.options.rocksdb:
             self.requires("rocksdb/10.5.1")
+        self.requires("secp256k1/0.7.1", transitive_headers=True)
+        self.requires("sqlite3/3.53.0", force=True)
         self.requires("xxhash/0.8.3", transitive_headers=True)
 
     exports_sources = (
